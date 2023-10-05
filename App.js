a071eac435ed4215f415963705f6753b9a7260b4
RamiMakdes <ramimakdes@gmail.com>
2023-10-05 22:42:08 +0200

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,87 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, FlatList, SafeAreaView, TouchableOpacity } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
+const Stack = createStackNavigator();
+
+const data = [
+  {id: '1', name: 'Apelsinjuice'},
+  {id: '2', name: 'Banansmoothie'},
+  {id: '3', name: 'Cider'},
+];
+
+const HomeScreen = ({ navigation }) => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={data}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View style={styles.itemContainer}>
+            <Text style={styles.itemText}>{item.name}</Text>
+            {item.name === 'Cider' && (
+              <TouchableOpacity
+                onPress={() => {
+                  navigation.navigate('ReadMore');
+                }}
+              >
+                <Text style={styles.ReadMore}>Read More</Text>
+              </TouchableOpacity>
+            )}
+          </View>
+        )}
+      />
+    </SafeAreaView>
+  );
+};
+
+const ReadMore = () => {
+  return (
+    <View style={styles.ReadMoreContainer}>
+    <Text style={styles.ReadMoreText}> Hej Hej! - Rami :D</Text>
     </View>
   );
-}
+};
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Home' component={HomeScreen} />
+        <Stack.Screen name='ReadMore' component={ReadMore} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  itemContainer: {
+    padding: 20,
+  },
+  itemText: {
+    fontSize: 30,
+  },
+  ReadMoreContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'blue',
+  },
+  ReadMoreText: {
+    fontSize: 50,
+    color: 'white',
+  },
+  ReadMore: {
+    color: 'blue',
+    marginTop: 10,
   },
 });
+
+export default App;
